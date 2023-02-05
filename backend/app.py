fa1a3d43834cca09f2088bf84f0ea9c7edadc941
adshayan <adshayan.b@gmail.com>
2023-02-05 12:58:27 -0500

    Add anaylstics table and count

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY']='secret-key'
 regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token=None
+        if 'x-access-tokens' in request.headers:
+            token=request.headers['x-access-tokens']
+        if not token:
+            return jsonify(message='Token is missing'),401
+        try:
+            data=jwt.decode(token, app.config['SECRET_KEY'])
+            current_user=User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify(message='Token is invalid'),401
 
+        return f(current_user, *args, **kwargs)
+    return decorated
 
 db=SQLAlchemy(app)
 @app.cli.command('dbCreate')
@@ -61,6 +76,11 @@ class User(db.Model):
     lastName=Column(String(50))
     email=Column(String(50), unique=True)
     password=Column(String(50))
+
+class Analytics(db.Model):
+    id=Column(Integer, primary_key=True)
+    user_id=Column(String(50), unique=True)
+    count = Column(Integer)
     
 @app.route('/api/register', methods=['POST'])
 def register():
@@ -106,3 +126,27 @@ def login():
         return jsonify(token=token.decode('UTF-8'))
     else:
         return jsonify(message='Your email or password is incorrect'),401
+
+@app.route('/api/count', methods=['POST'])
+@token_required
+def analysis(current_user):
+    json_data = request.get_json()
+    distractions = int(json_data['distractions'])
+
+    user_data={}
+    user_data['public_id']=current_user.public_id
+
+    userMetric=Analytics.query.filter_by(user_id=user_data['public_id']).first()
+
+    if userMetric:
+        userMetric.count = userMetric.count + distractions
+        db.session.commit()
+        return (jsonify(message="Data updated"))
+    else:
+        newTrackData=Analytics(
+        user_id=user_data['public_id'],
+        count = distractions
+        )
+        db.session.add(newTrackData)
+        db.session.commit()
+        return jsonify(message='Data Added'),201
