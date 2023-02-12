f6f1a954a94acdeee5a4fdd459df46f03cce7496
Soumil Dharaskar <soumil.dharaskar@hotmail.com>
2023-02-11 20:44:06 -0500

    Added past trips page layout to test development functionality

--- frontend/screens/CreateSession.js
+++ frontend/screens/CreateSession.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+// import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  SectionList,
+  StatusBar,
+} from "react-native";
 
-export default function CreateSession() {
-  return (
-    <View style={styles.container}>
-      <Text>Track My Trip</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function CreateSession() {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <SectionList
+//         sections={DATA}
+//         keyExtractor={(item, index) => item + index}
+//         renderItem={({ item }) => (
+//           <View style={styles.container}>
+//             <Text>Track My Trip</Text>
+//             <StatusBar style="auto" />;
+//           </View>
+//         )}
+//         renderSectionHeader={({ section: { title } }) => (
+//           <Text style={styles.header}>{title}</Text>
+//         )}
+//       />
+//     </SafeAreaView>
+//   );
+// }
+
+const DATA = [
+  {
+    title: "Past Trips",
+    data: ["Trip 1", "Trip 2", "Trip 3"],
+  },
+  // {
+  //   title: "Sides",
+  //   data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  // },
+  // {
+  //   title: "Drinks",
+  //   data: ["Water", "Coke", "Beer"],
+  // },
+  // {
+  //   title: "Desserts",
+  //   data: ["Cheese Cake", "Ice Cream"],
+  // },
+];
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.title}>{item}</Text>
+        </View>
+      )}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+    />
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: "#fff",
+    padding: 60,
+    marginVertical: 30,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
   },
 });
+
+export default App;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
