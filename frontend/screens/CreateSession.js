30df6db954316cea80eb4bf146b542491da2d7bf
Soumil Dharaskar <soumil.dharaskar@hotmail.com>
2023-03-07 19:03:04 -0500

    Changed all double quotes to single quotes in CreateSession.js

--- frontend/screens/CreateSession.js
+++ frontend/screens/CreateSession.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
-import CustomButton from "../components/button/custom-button";
-import CustomCard from "../components/card/custom-card";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import CustomButton from '../components/button/custom-button';
+import CustomCard from '../components/card/custom-card';
 
 const App = () => {
-  const [text, setText] = useState("Message Prompt");
-  const onPressStart = (event) => setText("Session Started");
-  const onPressStop = (event) => setText("Session Stopped");
+  const [text, setText] = useState('Message Prompt');
+  const onPressStart = (event) => setText('Session Started');
+  const onPressStop = (event) => setText('Session Stopped');
 
   return (
     <SafeAreaView style={styles.createContainer}>
@@ -27,11 +27,11 @@ const App = () => {
       >
         <View style={styles.buttonsContainer}>
           <CustomButton
-            type="emphasized"
-            text={"Start"}
+            type='emphasized'
+            text={'Start'}
             onPress={onPressStart}
           />
-          <CustomButton type="emphasized" text={"Stop"} onPress={onPressStop} />
+          <CustomButton type='emphasized' text={'Stop'} onPress={onPressStop} />
         </View>
       </CustomCard>
     </SafeAreaView>
@@ -41,54 +41,54 @@ const App = () => {
 const styles = StyleSheet.create({
   createContainer: {
     flex: 1,
-    backgroundColor: "#fffbf6",
+    backgroundColor: '#fffbf6',
   },
   item: {
     padding: 60,
     marginVertical: 30,
-    alignSelf: "center",
-    height: "30%",
+    alignSelf: 'center',
+    height: '30%',
   },
   header: {
     fontSize: 32,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
   title: {
     fontSize: 26,
-    color: "#EF5350",
-    fontWeight: "500",
+    color: '#EF5350',
+    fontWeight: '500',
   },
   upperContainer: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
     paddingTop: 20,
     paddingBottom: 10,
   },
   textWrapper: {
-    width: "85%",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
+    width: '85%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
     marginTop: 20,
   },
   headerTitle: {
     fontSize: 30,
-    fontWeight: "600",
-    color: "#3f2021",
+    fontWeight: '600',
+    color: '#3f2021',
   },
   buttonsContainer: {
-    display: "flex",
-    flexDirection: "row",
-    backgroundColor: "#fff",
-    justifyContent: "space-evenly",
-    alignSelf: "center",
-    width: "85%",
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    justifyContent: 'space-evenly',
+    alignSelf: 'center',
+    width: '85%',
   },
   lowerOuterContainer: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 0,
     borderTopLeftRadius: 25,
@@ -99,12 +99,12 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   lowerInnerContainer: {
-    justifyContent: "space-between",
-    alignItems: "flex-start",
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
     minHeight: 400,
   },
   startButton: {
-    backgroundColor: "blue",
+    backgroundColor: 'blue',
   },
 });
 
