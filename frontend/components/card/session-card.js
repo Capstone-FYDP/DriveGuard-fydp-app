7c04d55c672ee2ea773259188c2e5182dcb593df
Preyansh Kachhia <pkachhia@uwaterloo.ca>
2023-05-12 18:09:34 -0400

    feat: fetched  get session api and added ui for sessions page

--- frontend/components/card/session-card.js
+++ frontend/components/card/session-card.js
@@ -1,10 +1,20 @@
 import React, { useContext } from 'react';
+import { View, Text } from 'react-native';
 import { MainContext } from '../../context/MainContext';
 
 const SessionCard = (props) => {
     const {image, startDate, duration, status, numOfIncidents} = props
 
     const context = useContext(MainContext);
+
+    return (
+        <View>
+            <Text>{startDate}</Text>
+            <Text>Duration: {duration[0]} hour(s) {duration[1]} min(s)</Text>
+            <Text>{status}</Text>
+            <Text>{numOfIncidents}</Text>
+        </View>    
+    );
 }
 
 export default SessionCard
--- frontend/screens/MySessions.js
+++ frontend/screens/MySessions.js
@@ -1,11 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { MainContext } from '../context/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
+import LoadingIndicator from '../components/loadingIndicator/loadingIndicator';
+import SessionCard from '../components/card/session-card';
 
-export default function MySessions() {
+const MySessions = () => {
+  const context = useContext(MainContext);
+  const [sessions, setSessions] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const getToken = async () => {
+    try {
+      return await AsyncStorage.getItem('auth_token');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getSessions = async () => {
+    try {
+      const token = await getToken();
+      const response = await fetch(
+        context.fetchPath + `api/getSessions`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-access-tokens': token,
+          },
+        }
+      );
+      const json = await response.json();
+
+      if (json.message) {
+        Toast.show({
+          text1: 'Error',
+          text2: json.message,
+          type: 'error',
+        });
+      } else {
+        console.log(json.sessionData);
+        setSessions(json.sessionData);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getSessions();
+  }, []);
+
+  //Display the duration in hours and mins -> output is array [hours, mins]
+  const getTimeDuration = (startDate, endDate) => {
+    let start = new Date(startDate);
+    let end = new Date(endDate);
+
+    const totalMin = Math.abs(start.getTime() - end.getTime()) / (1000 * 60)
+ 
+    const hours = (totalMin / 60);
+    const roundHours = Math.floor(hours);
+    const minutes = (hours - roundHours) * 60;
+    const roundMinutes = Math.round(minutes);
+    return [roundHours, roundMinutes]
+  }
+
   return (
     <View style={styles.container}>
-      <Text>My Trips</Text>
-      <StatusBar style="auto" />
+      <View style={styles.upperContainer}>
+        <View style={styles.textWrapper}>
+          <Text style={styles.headerTitle}>My Trips</Text>
+        </View>
+      </View>
+      {isLoading ? (
+        <LoadingIndicator isAnimating={true} />
+      ) : (
+        <View style={styles.flatListContainer}> 
+          <FlatList
+            data={sessions.reverse()}
+            renderItem={({item}) => {
+              return(
+                <SessionCard startDate={item.startDate} duration={getTimeDuration(item.startDate, item.endDate)} status={item.status} numOfIncidents={item.numOfIncidents} />
+              )
+            }}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +97,33 @@ export default function MySessions() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffbf6',
+  },
+  upperContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20,
+    paddingBottom: 10,
+  },
+  textWrapper: {
+    width: '85%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  headerTitle: {
+    fontSize: 30,
+    fontWeight: '600',
+    color: '#3f2021',
+  },
+  flatListContainer: {
+    width: '85%',
+    justifyContent: 'space-around',
+    alignSelf: 'center',
   },
 });
+
+export default MySessions;
