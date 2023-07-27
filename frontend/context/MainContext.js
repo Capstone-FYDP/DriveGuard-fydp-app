20440bd23f8731750fc5a54400cdceb3e9e76a2e
Preyansh Kachhia <pkachhia@uwaterloo.ca>
2023-07-27 07:34:14 -0400

    fix: revamped entire colour scheme in context, applied it across entire app

--- frontend/context/MainContext.js
+++ frontend/context/MainContext.js
@@ -1,20 +1,26 @@
-import React, { createContext, useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { createContext, useState } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 export const Stack = createStackNavigator();
 export const MainContext = createContext();
 
 export const MainProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
-  const [gradient, setGradient] = useState(['#f62e4a', '#ff8d4f']);
-  const fetchPath = 'http://192.168.0.154:5000/';
+  const [theme, setTheme] = useState("light");
+  const fetchPath = "http://192.168.0.159:5000/";
+  const primaryColour = "#1f527b";
+  const secondaryColour = "#17435E";
+  const tertiaryColour = "#2c79b3";
+  const screenBackground = "#F4F8FB";
 
   return (
     <MainContext.Provider
       value={{
         theme,
         setTheme,
-        gradient,
         fetchPath,
+        primaryColour,
+        secondaryColour,
+        tertiaryColour,
+        screenBackground,
       }}
     >
       {children}
